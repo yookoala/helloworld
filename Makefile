59210b301ad3f320d67b2236e53d967985ceff30
Koala Yeung <koalay@gmail.com>
2014-04-21 11:59:54 +0800

    Added a test that make sense
    Assert the expected result of hello library

--- Makefile
+++ Makefile
@@ -10,10 +10,5 @@ clean:
 
 test: compile
 	cd tests; $(MAKE) $@
-	@ echo
-	@ echo ----------
-	@ echo  Run Test
-	@ echo ----------
-	@ ./tests/test && echo Pass
 
 .PHONY: compile all clean test
--- tests/test_hello.c
+++ tests/test_hello.c
@@ -1,5 +1,17 @@
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
 #include "hello.h"
 
 int main() {
+	char* result;
+	result = Hello("World");
+
+	// compare the two string and see if they are the same
+	assert(strcmp(result, "Hello World") == 0);
+
+	// free memory
+	free(result);
+
 	return 0;
 }
