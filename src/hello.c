51d493ece159b0884bd3cb4e3bed1845cc116c24
Koala Yeung <koalay@gmail.com>
2014-04-21 12:01:26 +0800

    Finish the basic feature
    Finish the feature that fulfills the unit test

--- src/hello.c
+++ src/hello.c
@@ -3,7 +3,9 @@
 #include "hello.h"
 
 char* Hello(char* target) {
-	char* str_return = "";
+	char* str_return = (char*) malloc(strlen(target) + 6);
+	strcpy(str_return, "Hello ");
+	strcat(str_return, target);
 	return str_return;
 }
 
